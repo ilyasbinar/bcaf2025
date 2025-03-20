35f1f5d12da1d3823cbfff27c0dd64fdad0af865
Ilyas <ilyas@lua.co.id>
2025-03-20 11:25:10 +0700

    Ilyas

--- java-fundamental/src/Ilyas.java
+++ java-fundamental/src/Ilyas.java
@@ -1,17 +1,17 @@
 public class Ilyas {
-    public void soal1a(int jumlahAngka){
-        //print 2 4 8 16 32 64
+    public void soal1a(int jumlahAngka, int angka1){
+        //print sejumlah jumlahAngka dikalikan 2 dimulai dari angka1
     }
 
-    public void soal1b(int jumlahAngka){
-
+    public void soal1b(int jumlahAngka, int angka1, int angka2){
+        //print angka hasil dari perkalian dua angka sebelumnya sejumlah jumlahAngka
     }
 
     public void soal2(int[] angkaAcak){
-
+        //print urutkan angka dari array of int
     }
 
     public void soal3(char[] hurufAcak){
-
+        //print urutkan huruf dari array of char
     }
 }
