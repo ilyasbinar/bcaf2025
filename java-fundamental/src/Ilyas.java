c4ec9650710e077758c75aa7f38f9eb5112bdb77
Ilyas <ilyas@lua.co.id>
2025-03-20 10:46:56 +0700

    Ilyas

---
+++ java-fundamental/src/Ilyas.java
@@ -0,0 +1,17 @@
+public class Ilyas {
+    public void soal1a(int jumlahAngka){
+        //print 2 4 8 16 32 64
+    }
+
+    public void soal1b(int jumlahAngka){
+
+    }
+
+    public void soal2(int[] angkaAcak){
+
+    }
+
+    public void soal3(char[] hurufAcak){
+
+    }
+}
